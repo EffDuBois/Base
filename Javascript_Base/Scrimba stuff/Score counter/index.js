032ea83a218b78a062afa76de2cb70386d619fd7
effdubois <arnabkar2003@protonmail.com>
2022-10-25 15:34:41 +0530

    updated the UI a little

--- Javascript_Base/Scrimba stuff/Score counter/index.js
+++ Javascript_Base/Scrimba stuff/Score counter/index.js
@@ -1,35 +1,33 @@
-let home = document.getElementById("score-home")
-let guest = document.getElementById("score-guest")
-let leaderEl= document.getElementById("leader-el")
+let home = document.getElementById("score-home");
+let away = document.getElementById("score-away");
+let leaderEl = document.getElementById("leader-el");
 let score = {
-    home: 0,
-    guest: 0
-}
+  home: 0,
+  away: 0,
+};
 
 function increasehome(amt) {
-    score.home+=amt
-    home.textContent = score.home
-    leader()
-}               
-function increaseGuest(amt) {
-    score.guest+=amt
-    guest.textContent = score.guest
-    leader()
-}      
+  score.home += amt;
+  home.textContent = score.home;
+  leader();
+}
+function increaseAway(amt) {
+  score.away += amt;
+  away.textContent = score.away;
+  leader();
+}
 function reset() {
-    score.guest = 0
-    score.home = 0
-    home.textContent = score.home
-    guest.textContent = score.guest
+  score.away = 0;
+  score.home = 0;
+  home.textContent = score.home;
+  away.textContent = score.away;
 }
 function leader() {
-    if (score.home>score.guest){
-        leaderEl.textContent = "Leader: Home"
-    }
-    else if (score.home<score.guest){
-        leaderEl.textContent = "Leader: Guest"
-    }
-    else {
-        leaderEl.textContent = "Leader: None"
-    }
+  if (score.home > score.away) {
+    leaderEl.textContent = "Leader: Home";
+  } else if (score.home < score.away) {
+    leaderEl.textContent = "Leader: Away";
+  } else {
+    leaderEl.textContent = "Leader: None";
+  }
 }
